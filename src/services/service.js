6d047962823be066834c394017b8cf25ae51b38e
amitavlivoso <amitav@livosotech.com>
2025-07-29 13:11:24 +0530

    .

--- src/services/service.js
+++ src/services/service.js
@@ -1,8 +1,11 @@
 import axiosClient from "./axiosClient";
 
-export function login(payLoad) {
-  return axiosClient.post("/auth/login", payLoad);
-}
+
 export function signup(payLoad) {
   return axiosClient.post("/auth/signup", payLoad);
 }
+
+export function login(payLoad) {
+  return axiosClient.post("/auth/login", payLoad);
+}
+
